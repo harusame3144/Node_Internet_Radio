460b15e49bfd1947355dc25f01a2428df3cbe8b0
Sangoon_Is_Noob <40298015+cotwo0139@users.noreply.github.com>
2019-06-29 18:57:00 +0900

    Updated

---
+++ index.js
@@ -0,0 +1,99 @@
+﻿const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const Station = require('@kefir100/radio-engine');
+const fs = require('fs');
+const station = new Station.Station({
+  error: (...args) => { }
+});
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set("views", __dirname + "/views");
+app.use("/assets", express.static(__dirname + '/public'));
+const files = fs.readdirSync('./Musics');
+files.forEach(item => {
+  station.addTrack({ path: './Musics', file: item });
+});
+
+app.get('/stream', (req, res) => {
+  station.connectListener(req, res);
+});
+
+app.get('/', (req, res) => {
+  res.render("index", { data: getInfo() });
+});
+
+io.on('connection', function (socket) {
+  socket.once("loaded", () => {
+    socket.emit("music_updated", getInfo());
+  })
+});
+
+let currentTrack;
+station.on('nextTrack', track => {
+  currentTrack = track;
+  io.emit("music_updated", getInfo())
+  console.log(currentTrack);
+});
+
+app.get('/info', (req, res) => {
+  res.json(getInfo());
+});
+
+station.start({
+  shuffle: true,
+});
+
+server.listen(1543, () => {
+  console.log("Server On");
+});
+
+function getInfo() {
+  const raw = currentTrack.getMeta().raw;
+  const data = {};
+  data.playlist = station.playlist.map(el => {
+    return {
+      name: el.track.fsStats.name,
+      duration: el.track.fsStats.duration
+    }
+  });
+  const array = currentTrack.fsStats.name.split(".mp3");
+  const title = array[0] + ` - [${secToHHMMSS(currentTrack.fsStats.duration / 1000)}]`
+  if (raw.APIC) {
+    data.nowplaying = {
+      title: title,
+      track: currentTrack,
+      raw: {
+        TIT2: raw.TIT2
+      },
+      image: {
+        type: raw.APIC.mime,
+        data: raw.APIC.imageBuffer.toString("base64")
+      }
+    }
+    return data;
+  } else {
+    data.nowplaying = {
+      title: title,
+      track: currentTrack,
+      raw: raw,
+      image: null
+    };
+    return data;
+  }
+}
+
+function secToHHMMSS(second) {
+  if (isNaN(second)) return "Live";
+  if (second === 0 || second === "0") return "00:00";
+  let sec_num = parseInt(second, 10); // don't forget the second param
+  let hours = Math.floor(sec_num / 3600);
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  let seconds = sec_num - (hours * 3600) - (minutes * 60);
+  if (hours < 10) {hours = "0" + hours;}
+  if (minutes < 10) {minutes = "0" + minutes;}
+  if (seconds < 10) {seconds = "0" + seconds;}
+  if (hours <= 0) {return minutes + ':' + seconds;} else {return hours + ':' + minutes + ':' + seconds;}
+} 
---
+++ public/stream.js
@@ -0,0 +1,95 @@
+'use strict';
+var io = io.connect();
+
+io.on("music_updated", async (data) => {
+    console.log(data);
+    audio.src = "/stream";
+    await audio.load();
+    await audio.play();
+    document.querySelector("#btn_group > button.titlebtn").innerText = data.nowplaying.title;
+});
+var myVar;
+async function myFunction() {
+    var audio = document.getElementById("audio");
+    audio.src = "/stream";
+    await audio.load();
+    var context = new AudioContext();
+    audio.crossOrigin = "anonymous";
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+    myVar = setTimeout(function () { showPage(context) }, 1000);
+    var canvas = document.getElementById("canvas");
+    // canvas.width = window.innerWidth / 2;
+    // canvas.height = window.innerHeight / 2;
+    var ctx = canvas.getContext("2d");
+
+    src.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.fftSize = 8192;
+
+    var bufferLength = analyser.frequencyBinCount;
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength)+3;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+        requestAnimationFrame(renderFrame);
+
+        x = 0;
+
+        analyser.getByteFrequencyData(dataArray);
+
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+        for (var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+
+            var r = barHeight + (25 * (i / bufferLength));
+            var g = 250 * (i / bufferLength);
+            var b = 50;
+
+            // ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+            ctx.fillStyle = "#7289DA";
+            ctx.fillRect(x, barHeight / HEIGHT, barWidth, barHeight / 3);
+
+            x += barWidth + 1;
+        }
+    }
+    renderFrame();
+};
+
+function showPage(context) {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("main").style.display = "table";
+    var audio = document.getElementById("audio");
+    const promise = audio.play();
+    if (promise !== undefined) {
+        promise.then(_ => {
+            console.log("Autoplay Started");
+        }).catch(error => {
+            location.reload();
+        });
+    }
+    context.resume().then(() => {
+        io.emit("loaded");
+        console.log('Playback resumed successfully');
+    });
+}
+
+function changePauseStatus() {
+    var audio = document.getElementById("audio");
+    if (audio.paused) {
+        document.querySelector("#btn_group > button").innerHTML = '<i class="fas fa-pause"></i>'
+        audio.play();
+    } else {
+        document.querySelector("#btn_group > button").innerHTML = '<i class="fas fa-play"></i>'
+        audio.pause();
+    }
+}
